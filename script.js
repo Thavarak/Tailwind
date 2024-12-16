26fc2eca61565384e34ce13980ff350e60319503
THAVARAK CHHOAN <thavarajchoan@gmail.com>
2024-12-16 15:54:12 +0700

    TW

---
+++ script.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 20,
+  slidesPerView: 1.2,
+  centeredSlides: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
